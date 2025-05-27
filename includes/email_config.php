015234ee0302df1118d0dcab16f15b62a3350147
Holly Schu <holly.schu14@gmail.com>
2025-05-27 13:10:02 -0700

    Ignore DB and email config files

--- includes/email_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-//NOT USING THIS ANYMORE  USING .ENV
-// $pmUserName = "postmaster@squalmband.com";
-// $pmPassword = "Web1212Squa!@!@";
-
-$pmUserName = "postmaster@hollyschu.com";
-$pmPassword = "Web1212Has!@!@";
